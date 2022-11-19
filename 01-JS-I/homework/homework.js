6f7763ebb879c90cd1ae5f5c66b8ce5609586fd1
elgabydecba007 <gabriel05cba.gb@gmail.com>
2022-11-19 19:46:27 -0300

    cambios con franco

--- 01-JS-I/homework/homework.js
+++ 01-JS-I/homework/homework.js
@@ -1,13 +1,13 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = 9;
+const nuevaString = "hola";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = 4;
+const nuevoNum = null;
 
 // Crea una variable booleana:
-const nuevoBool = 4 ;
+const nuevoBool = null ;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - null === 5;
